44ee7783b77e6ea681e262cdd0b63f5c2c665658
IvanTKOP <thekillerofporters@gmail.com>
2021-04-12 01:56:27 +0200

    adivinaNumero

---
+++ RepasoCurso/01Formularios/adivinaNumeroInicio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adivina Número</title>
+</head>
+<body>
+    <h1>Juego Adivina Número</h1>
+    <p>Jugador 1, escribe el número que tendrá que adivinar el jugador 2</p>
+    <form method="get" action="adivinaNumeroPrincipal.php">
+        <input type="number" name="numeroOculto">
+        <input type="submit" value="Empezar juego">
+    </form>
+</body>
+</html>
---
+++ RepasoCurso/01Formularios/adivinaNumeroPrincipal.php
@@ -0,0 +1,79 @@
+<?php
+
+if (isset($_REQUEST["numeroOculto"])) {
+    $numeroOculto = $_REQUEST["numeroOculto"];
+}
+
+if (isset($_REQUEST["numeroIntento"])) {
+    $numeroIntento = $_REQUEST["numeroIntento"];
+    $intentos = $_REQUEST["intentos"] + 1;
+    if ($intentos == 10 && $numeroIntento != $numeroOculto) { // &&: caso raro de acertar en el último intento
+        $terminado = true;
+        $maxIntentos = true;
+        $numeroIntento = null;
+    } else {
+        $terminado = false;
+        $maxIntentos = false;
+    }
+} else {
+    $numeroIntento = null;
+    $intentos = 0;
+    $terminado = false;
+    $maxIntentos = false;
+}
+
+$intentosRestantes = 10 - $intentos;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adivina Número</title>
+</head>
+<body>
+<?php
+if ($numeroIntento != null) {
+    if ($numeroIntento == $numeroOculto) {?>
+            <h2>¡Correcto, has ganado!</h2>
+            <p>Has realizado <?php echo "$intentos" ?> intentos</p>
+            <?php $terminado = true;
+    }
+    if ($numeroIntento > $numeroOculto) {?>
+                <h2>Casi, prueba un número más bajo</h2>
+            <?php if (($numeroIntento - $numeroOculto) <= 5) {?>
+                <h2>Distancia: *</h2>
+                <?php } else if (($numeroIntento - $numeroOculto) <= 10) {?>
+                    <h2>Distancia: **</h2>
+                    <?php } else if (($numeroIntento - $numeroOculto) > 10) {?>
+                    <h2>Distancia: ***</h2>
+                    <?php }
+    }
+    if ($numeroIntento < $numeroOculto) {?>
+                <h2>Casi, prueba un número más alto</h2>
+                <?php if (($numeroOculto - $numeroIntento) <= 5) {?>
+                <h2>Distancia: *</h2>
+                <?php } else if (($numeroOculto - $numeroIntento) <= 10) {?>
+                    <h2>Distancia: **</h2>
+                    <?php } else if (($numeroOculto - $numeroIntento) > 10) {?>
+                    <h2>Distancia: ***</h2>
+                    <?php }
+    }
+}
+if (!$terminado) {?>
+    <p>Jugador 2, prueba a adivinar el número, tienes <?php echo "$intentosRestantes" ?> intentos</p>
+    <form method="get" action="adivinaNumeroPrincipal.php">
+        <input type="hidden" value="<?=$numeroOculto?>" name="numeroOculto">
+        <input type="hidden" value="<?=$intentos?>" name="intentos">
+        <input type="number" name="numeroIntento">
+        <input type="submit" value="Adivinar">
+    </form>
+    <?php }
+
+if ($maxIntentos) {?>
+    <h1>Has perdido, se acabaron los intentos.</h1>
+<?php }
+?>
+</body>
+</html>
