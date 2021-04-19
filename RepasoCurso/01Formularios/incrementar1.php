b9891f177f207d0f55fe47f7a10a69353b39b366
Ivan Rodríguez <thekillerofporters@gmail.com>
2021-04-19 13:29:56 +0200

    solucion incrementar final

--- RepasoCurso/01Formularios/incrementar1.php
+++ RepasoCurso/01Formularios/incrementar1.php
@@ -3,7 +3,11 @@ $acumulado = 0; // Apostamos a que acabamos de llegar o piden reiniciar;
 
 // Si ya viene un acumulado *Y* NO nos piden reiniciar, hay que recoger y utilizar el valor que viene.
 if (isset($_COOKIE["acumulado"]) && !isset($_REQUEST["reiniciar"])) {
-    $acumulado = $_COOKIE["acumulado"] + 1; // Recogemos y sumamos 1.
+    $acumulado = $_COOKIE["acumulado"];
+
+    if (isset($_REQUEST["sumar"])) { // si viene sumar, sumamos 1
+        $acumulado++;
+    }
 }
 
 // Establecemos la cookie con el valor que haya quedado:
@@ -19,7 +23,7 @@ setcookie("acumulado", $acumulado, time() + 60 * 60);
 <body>
     <p><?=$acumulado?></p>
     <form action="incrementar1.php" method="get">
-        <input type="submit" value="+1">
+        <input type="submit" value="+1" name="sumar">
         <input type="submit" value="Reiniciar" name="reiniciar"> <!--manera menos eficiente -->
         <a href='<?=$_SERVER["PHP_SELF"]?>?reiniciar'>Manera eficiente de reiniciar</a> <!--manera más eficiente -->  <!-- Hacemos lo mismo que submit pero con $_SERVER -->
     </form>
